6ac290d308947c0b93b706d3b44e054e5a8d7c86
Ngo Zong Han <zonghan01@gmail.com>
2025-05-31 15:26:45 +0800

    removing ios google key

--- sc2006_trash/ios/Runner/AppDelegate.swift
+++ sc2006_trash/ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyCzRqrMdcBC9ZojuoPS_s4cLV851zWROwQ")
+    GMSServices.provideAPIKey("")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
